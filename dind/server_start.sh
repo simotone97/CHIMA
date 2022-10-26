670ee90983407e22c38d35aa42a44110444b9af4
simotone97 <50499875+simotone97@users.noreply.github.com>
2022-10-26 17:06:03 +0200

    Update server_start.sh
    this should solve the problem of server starting

--- dind/server_start.sh
+++ dind/server_start.sh
@@ -5,12 +5,12 @@ mount -t debugfs none /sys/kernel/debug
 dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 &
 cd CHIMAclient
 
-while ! ls /sys/class/net | grep -e "-eth0" > /dev/null;
+while ! ifconfig | grep -o -E ".+?-eth0"
 do
     sleep 5
 done
 
-python3 main.py -i $(ls /sys/class/net | grep -e "-eth0") &
+python3 main.py -i $(ifconfig | grep -o -E ".+?-eth0") &
 
 while ! DOCKER_HOST=localhost:2375 docker load < /python_alpine.tar;
 do
